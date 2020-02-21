cb61542bc501c622253d870b13395596a11d651d
Jacob and Nathan MacLeod <48350114+jacob-and-nathan@users.noreply.github.com>
2020-02-21 14:42:05 +0000

    Update main.py

--- main.py
+++ main.py
@@ -6,42 +6,64 @@ class Block :
   #Initialize values relating to the blockchain
   index = 0
   data = None
-  h = 'Genesis'
+  hash = None
   timestamp = datetime.datetime.now()
   previousHash = 0x0
+  next = None
+  #Number only sored once, or as I like to call it, 'incrementing variable'
+  nonce = 0
 
   def __init__(self, data) :
     self.data = data
 
 #Convert all the values to a hash using the hashlib library
   def hashBlock () :
-    h = hashlib.sha256()
-    h.update (
+    hash = hashlib.sha256()
+    hash.update (
+      str(Block.nonce).encode('utf-8') +
       str(Block.index).encode('utf-8') +
       str(Block.data).encode('utf-8') +
       str(Block.previousHash).encode('utf-8') +
       str(Block.timestamp).encode('utf-8') 
     )
     #Return the hash
-    return h.hexdigest()
+    return hash.hexdigest()
 
 
 #Create a new blockchain class
-class blockchain :
+class Blockchain :
+  #The maximum nonce
+  maxNonce = 2**32
+
+  #mining difficulty variable
+  diff = 14
+
+  #Target hash
+  target = 2**(256-diff)
+
+  #This is the Genesis block
   block = Block("Genesis")
+
+  #Stores the newest addition to the block
+  head = block
+
   #Create a new block in the blockchain
   def generateBlock (block) :
-    #Set the values
+    #Change some values
     Block.previousHash =  Block.hashBlock()
     Block.index = Block.index + 1
-    Block.data = block
-    #Calculate the hash
-    hash = Block.hashBlock()
-    #--Print values for debugging purposes--#
-    #print ("Previous Hash\n")
-    #print (Block.previousHash)
-    #print ("New hash\n")
-    #print (hash)
+    Block.next = block
+    Blockchain.block = Block.next
 
-    #Return the hash
-    return hash
+  def mine (self, block) :
+    #Repeat maxNonce times
+    for n in range(blockchain.maxNonce) :
+      #Check if the proposed block is to be accepted
+      if int(Block.hashBlock(), 16) <= blockchain.target :
+        Blockchain.generateBlock(block)
+        break
+      else :
+        #Increase the nonce
+        Block.nonce = Block.nonce + 1
+
+blockchain = Blockchain()
